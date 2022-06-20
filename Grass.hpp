bd94dee4a4afcbbd5d2d296f11c18248aa54c245
Anton <ant17893@mail.ru>
2022-06-20 19:57:06 +0500

    Add Grass AntonB

---
+++ Grass.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef GRASS_HPP
+#define GRASS_HPP
+
+#include "Plant.hpp"
+#include "Seeds.hpp"
+
+class Grass:public Plant {
+public:
+	Grass(): Plant("Grass", 15) { }
+	Harvest::uPoiner harvest() {
+		decreaseHarvest();
+		return std::make_unique<Seeds>();
+	}
+};
+
+#endif // GRASS_HPP
---
+++ Seeds.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef SEEDS_HPP
+#define SEEDS_HPP
+
+#include "Harvest.hpp"
+
+class Seeds:public Harvest {
+public:
+	Seeds():Harvest("Seeds") { }
+private:
+	void setName(const char* name) {
+		Harvest::setName(name);
+	}
+};
+
+#endif // !SEEDS_HPP
+
